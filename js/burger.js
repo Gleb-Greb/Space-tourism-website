74cba68b6698926b5606237128b04eea761356bb
Gleb-Greb <gleb42199@gmail.com>
2023-03-10 14:42:02 +0200

    create home page

---
+++ js/burger.js
@@ -0,0 +1,30 @@
+let burger = document.querySelector(".burger")
+
+
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active")
+})
+console.log(burger);
+
+
+
+// function showBurger() {
+//     burger.classList.toggle("active")
+// }
+
+
+
+// let car = {
+//     model: "BMW",
+//     color: "red",
+//     year: 2020,
+//     hasOwner: true,
+//     engine: {
+//         year: 2017,
+//         power: 2000,
+//     },
+//     makeNoize : function() {
+//         console.log('wrooom!!!')
+//     }
+// }
